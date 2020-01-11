4b467c93320f6d36e8e2a24b2ce6b6531d378229
Vitor Torino <vitor12xx@gmail.com>
2020-01-11 02:21:55 +0000

    now Hell Bell

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Crash Course`,
+    title: `The Hell Bell`,
     description: `Sample webiste for Gatsby Crash Course of Traversy Media channel. Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
